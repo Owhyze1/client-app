d6bb9f85244a1233e00dd3d03a39c1a17f34fb53
Tiffany Wise <TiffanyJWise84@gmail.com>
2019-05-02 20:46:04 -0400

    Add react-native-gesture-handler and modified MainActivity.java

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, Text, Button, Alert} from 'react-native';
+import {StyleSheet, View, Text, Button} from 'react-native';
 
 export default class HomeScreen extends Component {
 
